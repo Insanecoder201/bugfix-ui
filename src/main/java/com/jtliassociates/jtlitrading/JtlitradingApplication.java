abb2c9ac8d1a7fd2f967e1a5d13e571b128cbecb
Insanecoder201 <insanecoder900@gmail.com>
2019-05-19 19:02:21 +0530

    Working js and css

---
+++ src/main/java/com/jtliassociates/jtlitrading/JtlitradingApplication.java
@@ -0,0 +1,13 @@
+package com.jtliassociates.jtlitrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtlitradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtlitradingApplication.class, args);
+	}
+
+}
